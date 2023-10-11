2e1c3f5c2e13848e31601609194966376908e01c
Ramoxa <ramoxa0007@yandex.ru>
2023-10-11 21:28:52 +0300

    Исправил с учетом правок

--- src/ReportEngine.java
+++ src/ReportEngine.java
@@ -9,26 +9,32 @@ public class ReportEngine {
     void readYearlyReports() {
         String fileName = "y.2021.csv";
         ArrayList<String> strings = fileReader.readFileContents(fileName);
-        strings.remove(0);
+        if (strings == null) {
+            System.out.println("В системе нет файла с годовым отчетом");
+            System.out.println("Загрузите файл с годовым отчетом");
+            Main.printMenu();
+        } else {
+            strings.remove(0);
 
-        for (String string : strings) {
-            String[] split = string.split(",");
-            int month = Integer.parseInt(split[0]);
-            int amount = Integer.parseInt(split[1]);
-            boolean isExpense = Boolean.parseBoolean(split[2]);
+            for (String string : strings) {
+                String[] split = string.split(",");
+                int month = Integer.parseInt(split[0]);
+                int amount = Integer.parseInt(split[1]);
+                boolean isExpense = Boolean.parseBoolean(split[2]);
 
-            YearlyRecord yearlyRecord = yearlyReport.records.get(month);
-            if (yearlyRecord == null) {
-                if (isExpense == true) {
-                    yearlyReport.records.put(month, new YearlyRecord(month, amount, 0));
+                YearlyRecord yearlyRecord = yearlyReport.records.get(month);
+                if (yearlyRecord == null) {
+                    if (isExpense == true) {
+                        yearlyReport.records.put(month, new YearlyRecord(month, amount, 0));
+                    } else {
+                        yearlyReport.records.put(month, new YearlyRecord(month, 0, amount));
+                    }
                 } else {
-                    yearlyReport.records.put(month, new YearlyRecord(month, 0, amount));
-                }
-            } else {
-                if (isExpense == true) {
-                    yearlyRecord.expense = amount;
-                } else {
-                    yearlyRecord.earning = amount;
+                    if (isExpense == true) {
+                        yearlyRecord.expense = amount;
+                    } else {
+                        yearlyRecord.earning = amount;
+                    }
                 }
             }
         }
@@ -38,25 +44,31 @@ public class ReportEngine {
         for (int i = 1; i <= 3; i++) {
             String filename = "m.20210" + i + ".csv";
             ArrayList<String> strings = fileReader.readFileContents(filename);
-            strings.remove(0);
-            MonthlyReport monthlyReport = new MonthlyReport();
-            monthlyReport.year = 2021;
-            monthlyReport.month = i;
-            monthlyReport.expenses = new ArrayList<>();
-            monthlyReport.earnings = new ArrayList<>();
+            if (strings == null) {
+                System.out.println("В системе нет файла с годовым отчетом");
+                System.out.println("Загрузите файл с годовым отчетом");
+                Main.printMenu();
+            } else {
+                strings.remove(0);
+                MonthlyReport monthlyReport = new MonthlyReport();
+                monthlyReport.year = 2021;
+                monthlyReport.month = i;
+                monthlyReport.expenses = new ArrayList<>();
+                monthlyReport.earnings = new ArrayList<>();
 
-            for (String string : strings) {
-                String[] split = string.split(",");
-                Record record = new Record(split[0],
-                        Integer.parseInt(split[2]),
-                        Integer.parseInt(split[3]));
-                if (split[1].equals("TRUE")) {
-                    monthlyReport.expenses.add(record);
-                } else {
-                    monthlyReport.earnings.add(record);
+                for (String string : strings) {
+                    String[] split = string.split(",");
+                    Record record = new Record(split[0],
+                            Integer.parseInt(split[2]),
+                            Integer.parseInt(split[3]));
+                    if (split[1].equals("TRUE")) {
+                        monthlyReport.expenses.add(record);
+                    } else {
+                        monthlyReport.earnings.add(record);
+                    }
                 }
+                monthlyReports.put(i, monthlyReport);
             }
-            monthlyReports.put(i, monthlyReport);
         }
     }
 
@@ -126,7 +138,7 @@ public class ReportEngine {
     }
 
 public  void check () {
-    if ((monthlyReports.isEmpty()) && yearlyReport.records.isEmpty()) {
+    if ((monthlyReports.isEmpty()) || yearlyReport.records.isEmpty()) {
         System.out.println("Нет файлов с месячными отчетами.");
         System.out.println("Для выполения операции сначала выполните команды 1 и 2.");
         Main.printMenu();
@@ -139,8 +151,8 @@ public  void check () {
             System.out.println("Расходы согласно месячному отчету " + monthlyReports.get(i).sumExpenses());
             System.out.println("Расходы согласно годовому отчету " + yearlyReport.records.get(i).expense);
 
-            if (monthlyReports.get(i).sumEarnings() == yearlyReport.records.get(i).earning);
-            else if (monthlyReports.get(i).sumExpenses() == yearlyReport.records.get(i).expense) {
+            if ((monthlyReports.get(i).sumEarnings() == yearlyReport.records.get(i).earning)
+            && (monthlyReports.get(i).sumExpenses() == yearlyReport.records.get(i).expense)) {
                 System.out.println("Отчеты сверены, ошибок не обнаружено");
             }
             else {
@@ -149,7 +161,6 @@ public  void check () {
 
         }
     }
-
-}
+    }
 
 }
